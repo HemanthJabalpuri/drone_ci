6effd19bfac3c1129771383d441d99f3de0a4866
Hemanth <hemanthjabalpuri@gmail.com>
2021-07-05 07:33:06 +0530

    Build Realme C12 Kernel

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir ~/work
 
 echo "===+++ Cloning kernel sources +++==="
 cd ~/work
-git clone --depth=1 https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b land-m-oss kernel
+git clone --depth=1 https://github.com/HemanthJabalpuri/android_kernel_realme_mt6765 -b android-10.0
 
 echo "===+++ Downloading toolchain +++==="
 mkdir toolchain && cd toolchain
@@ -17,7 +17,7 @@ git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linu
 echo "===+++ Building kernel +++==="
 cd ~/work/kernel
 mkdir out
-make O=out ARCH=arm64 msm8937_defconfig
+make O=out ARCH=arm64 RXM2185_defconfig
 
 PATH="$HOME/work/toolchain/clang/bin:$HOME/work/toolchain/gcc64/bin:$HOME/work/toolchain/gcc/bin:${PATH}" \
 make O=out \
@@ -32,5 +32,5 @@ echo "===+++ Compiler version +++==="
 grep "LINUX_COMPILER" out/include/generated/compile.h
 
 echo "===+++ Uploading kernel +++==="
-curl -T out/arch/arm64/boot/Image https://oshi.at
+curl -T out/arch/arm64/boot/Image.gz-dtb https://oshi.at
 curl -T out/arch/arm64/boot/mtk.dtb https://oshi.at
