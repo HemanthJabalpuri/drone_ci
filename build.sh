13fef7d3d8da7551c38bc050c5cec99b01ee726d
Hemanth <hemanthjabalpuri@gmail.com>
2021-07-12 23:19:42 +0530

    Use android-10.0 branch

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ if [ "$PBRP" = "y" ]; then
   DT_LINK="https://github.com/HemanthJabalpuri/android_device_infinix_X687 -b pbrp"
 else
   MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
-  DT_LINK="https://github.com/HemanthJabalpuri/android_device_infinix_X687 -b test2"
+  DT_LINK="https://github.com/HemanthJabalpuri/android_device_infinix_X687"
 fi
 
 echo " ===+++ Setting up Build Environment +++==="
