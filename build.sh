b4cd3f57256980d416a2e53187ce55c152eaad2d
Hemanth <hemanthjabalpuri@gmail.com>
2021-07-18 11:54:32 +0530

    Realme 6 twrp-11

--- build.sh
+++ build.sh
@@ -1,52 +1,44 @@
 #!/bin/bash
 # Just a basic script U can improvise lateron asper ur need xD 
 
-PBRP=n
-
 abort() { echo "$1"; exit 1; }
 
+MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
 DEVICE=RMX2001
+DT_LINK="https://github.com/HemanthJabalpuri/android_device_realme_RMX2001 -b android-11"
 DT_PATH=device/realme/$DEVICE
-REC=TWRP
-if [ "$PBRP" = "y" ]; then
-  REC=PBRP
-  MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-10.0"
-  DT_LINK="https://github.com/HemanthJabalpuri/android_device_realme_RMX2001 -b pbrp"
-else
-  MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
-  DT_LINK="https://github.com/HemanthJabalpuri/android_device_realme_RMX2001"
-fi
 
 echo " ===+++ Setting up Build Environment +++==="
-mkdir ~/twrp10
-cd ~/twrp10
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
+mkdir ~/twrp11 && cd ~/twrp11
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST -g default,-device,-mips,-darwin,-notdefault 
-repo sync -j$(nproc --all)
+repo init --depth=1 -u $MANIFEST
+repo sync
+repo sync #fix for twrp11 build error
 git clone --depth=1 $DT_LINK $DT_PATH
 
+echo " ===+++ Patching Recovery Sources +++==="
+cd bootable/recovery
+curl -sL https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6758038/0001-Provide-an-option-to-skip-compatibility.zip-check-a11.patch.txt | patch -p1 -b
+curl -sL https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6694299/0001-Super-as-Super-only.patch.txt | patch -p1 -b
+cd -
+
 echo " ===+++ Building Recovery +++==="
-rm -rf out
-source build/envsetup.sh
-echo " source build/envsetup.sh done"
 export ALLOW_MISSING_DEPENDENCIES=true
-lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
-echo " lunch omni_${DEVICE}-eng done"
+. build/envsetup.sh
+echo " source build/envsetup.sh done"
+lunch twrp_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch twrp_${DEVICE}-eng done"
 mka recoveryimage || abort " mka failed with exit status $?"
 echo " mka recoveryimage done"
 
 # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
 echo " ===+++ Uploading Recovery +++==="
-if [ "$PBRP" = "y" ]; then
-  version=$(cat bootable/recovery/variables.h | grep "define PB_MAIN_VERSION" | cut -d \" -f2)
-else
-  version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-fi
-OUTFILE=${REC}-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
+OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
 
 cd out/target/product/$DEVICE
 mv recovery.img ${OUTFILE%.zip}.img
@@ -55,5 +47,3 @@ zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 curl -T $OUTFILE https://oshi.at
 #curl -F "file=@${OUTFILE}" https://file.io
 #curl --upload-file $OUTFILE http://transfer.sh/
-curl -sL https://git.io/file-transfer | sh
-./transfer wet $OUTFILE
