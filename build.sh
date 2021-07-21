f648368719e8fe37f71d2fdd86ddde66d081f782
Hemanth <hemanthjabalpuri@gmail.com>
2021-07-21 13:43:17 +0530

    Use all patches

--- build.sh
+++ build.sh
@@ -2,11 +2,30 @@
 
 branch=arrow-11.0
 
-git clone --depth=1 https://github.com/HemanthJabalpuri/android_frameworks_base -b $branch
-cd android_frameworks_base
-curl -sL https://github.com/PotatoProject-next/frameworks_base/commit/5db62c3223a698657acafdefda323baa5e773d4c.patch | patch -p1
-git add -A
-git commit -m "https://github.com/PotatoProject-next/frameworks_base/commit/5db62c3223a698657acafdefda323baa5e773d4c.patch"
-git push https://HemanthJabalpuri:${PASSWORD}@github.com/HemanthJabalpuri/android_frameworks_base.git --all
+applypatch() {
+  patch=$1
+  patchrepo=$2
+  git clone --depth=1 https://github.com/HemanthJabalpuri/${patchrepo} -b $branch
+  cd ${patchrepo}
+  curl -sL ${patch} | git am
+  git push https://HemanthJabalpuri:${PASSWORD}@github.com/HemanthJabalpuri/${patchrepo}.git --all
+  cd -
+}
 
-echo done
+# VoLTE patches
+applypatch \
+  https://github.com/PotatoProject-next/frameworks_base/commit/5db62c3223a698657acafdefda323baa5e773d4c.patch \
+  android_frameworks_base 
+
+applypatch \
+  https://github.com/PotatoProject-next/frameworks_opt_net_wifi/commit/88773b8285d7962d0add6a9f55c63fc045beb677.patch \
+  android_frameworks_opt_net_wifi
+
+applypatch \
+  https://github.com/PotatoProject-next/frameworks_opt_net_ims/commit/d2ce9579fc2ba741faf26c56a8e076b5099ac897.patch \
+  android_frameworks_opt_net_ims
+
+# Required for booting
+applypatch \
+  https://github.com/phhusson/platform_external_selinux/commit/f3d5e2eb212ebd4189428d6adb915880573962f9.patch \
+  android_external_selinux
