bbf96af61e2f8aa89f3260ca825dd40786f1aa62
Hemanth <hemanthjabalpuri@gmail.com>
2021-07-21 11:23:36 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo $PASSWORD
-echo $@
+branch=arrow-11.0
+
+git clone --depth=1 https://github.com/HemanthJabalpuri/android_frameworks_base -b $branch
+cd android_frameworks_base
+curl -sL https://github.com/PotatoProject-next/frameworks_base/commit/5db62c3223a698657acafdefda323baa5e773d4c.patch | patch -p1
+git add -A
+git commit -m "https://github.com/PotatoProject-next/frameworks_base/commit/5db62c3223a698657acafdefda323baa5e773d4c.patch"
+git push https://HemanthJabalpuri:${PASSWORD}@github.com/HemanthJabalpuri/android_frameworks_base.git --all
+
 echo done
