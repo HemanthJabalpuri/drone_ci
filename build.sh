4a9e747cbd4c4494aaa1b34dfa8e02851175b913
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-06 13:30:35 +0530

    Use Linaro

--- build.sh
+++ build.sh
@@ -8,11 +8,12 @@ git clone --depth=1 https://github.com/99degree/android_kernel_m3note -b m3note_
 
 echo "===+++ Downloading toolchain +++==="
 mkdir toolchain && cd toolchain
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc64
+#git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc64
+git clone --depth=1 https://github.com/nathanchance/gcc-prebuilts -b aarch64-linaro-7.x gcc64
 
 echo "===+++ Building kernel +++==="
 cd ~/work/kernel
-export CROSS_COMPILE=$HOME/work/toolchain/gcc64/bin/aarch64-linux-android-
+export CROSS_COMPILE=$HOME/work/toolchain/gcc64/bin/aarch64-linaro-linux-android-
 export ARCH=arm64 && export SUBARCH=arm64
 make m3_note_defconfig
 make -j$(nproc --all)
