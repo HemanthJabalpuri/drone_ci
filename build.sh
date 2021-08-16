a99258fc4a450a4a232708842785386064a3c992
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-16 17:28:24 +0530

    Fix

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ echo " ===+++ Building Recovery +++==="
 export ALLOW_MISSING_DEPENDENCIES=true
 . build/envsetup.sh
 echo " source build/envsetup.sh done"
-lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
+lunch twrp_${DEVICE}-eng || abort " lunch failed with exit status $?"
 echo " lunch twrp_${DEVICE}-eng done"
 mka recoveryimage || abort " mka failed with exit status $?"
 echo " mka recoveryimage done"
