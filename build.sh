c71adaa4be58e022c295926dae372947fcf291c0
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-19 10:28:50 +0530

    SearchLibs

--- build.sh
+++ build.sh
@@ -4,54 +4,23 @@
 abort() { echo "$1"; exit 1; }
 
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
-DT_PATH=device/realme/RMX2185
-DT_LINK="https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185 -b test"
 
 echo " ===+++ Setting up Build Environment +++==="
 mkdir ~/twrp10
 cd ~/twrp10
-apt install openssh-server -y
+apt install openssh-server ripgrep -y
 apt update --fix-missing
-apt install openssh-server -y
-DEVICE=${DT_PATH##*\/}
+apt install openssh-server ripgrep -y
 
 echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST -g default,-device,-mips,-darwin,-notdefault 
 repo sync -j$(nproc --all)
-git clone --depth=1 $DT_LINK $DT_PATH
 
-echo " ===+++ Patching Recovery Sources +++==="
-cd bootable/recovery
-applyPatch() {
-  curl -sL $1 | patch -p1
-  [ $? != 0 ] && echo " Patch $1 failed" && exit
+echo " ===+++ Searching for buildable libs +++==="
+searchLib() {
+  echo "======+++ Searching $1 +++======"
+  ripgrep "$1" .
 }
-applyPatch https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6679948/0001-Provide-an-option-to-skip-compatibility.zip-check.patch.txt
-applyPatch https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6694299/0001-Super-as-Super-only.patch.txt
-#applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/6989323/0001-twrpapp-automatically-exclude-on-AB-devices.patch.txt
-applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/6991161/NotchFix.patch.txt
-applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/6990939/0001-String-fixes-and-added-some-shell-functions.patch.txt
-cd -
 
-echo " ===+++ Building Recovery +++==="
-rm -rf out
-source build/envsetup.sh
-echo " source build/envsetup.sh done"
-export ALLOW_MISSING_DEPENDENCIES=true
-lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
-echo " lunch omni_${DEVICE}-eng done"
-mka recoveryimage || abort " mka failed with exit status $?"
-echo " mka recoveryimage done"
-
-# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
-echo " ===+++ Uploading Recovery +++==="
-version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
-
-cd out/target/product/$DEVICE
-mv recovery.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
-
-curl -T $OUTFILE https://oshi.at
-#curl -F "file=@${OUTFILE}" https://file.io
-#curl --upload-file $OUTFILE http://transfer.sh/
+searchLib 'vendor.display.config@1.0'
+searchLib 'libdrm'
