0fb79dd5da4b322fb76289a9edda620be452130b
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-25 12:37:48 +0530

    Narzo 30A

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PBRP=n
 
 abort() { echo "$1"; exit 1; }
 
-DT_PATH=device/realme/RMX2195
+DT_PATH=device/realme/RMX3171
 if [ "$PBRP" = "y" ]; then
   REC=PBRP
   MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-10.0"
@@ -13,7 +13,7 @@ if [ "$PBRP" = "y" ]; then
 else
   REC=TWRP
   MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
-  DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX2195 -b test"
+  DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX3171 -b android-10.0"
 fi
 DEVICE=${DT_PATH##*\/}
 
