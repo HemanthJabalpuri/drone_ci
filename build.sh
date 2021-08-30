2583b291343a27988ec5aa5abd1404985812ce8c
Hemanth <hemanthjabalpuri@gmail.com>
2021-08-30 19:52:51 +0530

    C15 QCom UI2

--- build.sh
+++ build.sh
@@ -5,22 +5,22 @@ PBRP=n
 
 abort() { echo "$1"; exit 1; }
 
-DT_PATH=device/vivo/Y15S
+DT_PATH=device/realme/RMX2195
 if [ "$PBRP" = "y" ]; then
   REC=PBRP
   MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-10.0"
   DT_LINK="https://github.com/HemanthJabalpuri/twrp_infinix_X687 -b pbrp"
 else
   REC=TWRP
-  MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1"
-  DT_LINK="https://github.com/HemanthJabalpuri/twrp_vivo_Y15S -b twrp-5.1"
+  MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
+  DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX2195 -b android-11.0"
 fi
 DEVICE=${DT_PATH##*\/}
 
 echo " ===+++ Setting up Build Environment +++==="
-apt install openssh-server openjdk-8-jdk -y
+apt install openssh-server -y
 apt update --fix-missing
-apt install openssh-server openjdk-8-jdk -y
+apt install openssh-server -y
 mkdir ~/twrpBuilding && cd ~/twrpBuilding
 
 echo " ===+++ Syncing Recovery Sources +++==="
@@ -34,16 +34,16 @@ applyPatch() {
   curl -sL $1 | patch -p1
   [ $? != 0 ] && echo " Patch $1 failed" && exit
 }
-#applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2194/files/6997950/SkipTrebleCompatibility.patch.txt
-#applyPatch https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6694299/0001-Super-as-Super-only.patch.txt
-#applyPatch https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6758394/NotchFix.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2194/files/6997950/SkipTrebleCompatibility.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6694299/0001-Super-as-Super-only.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6758394/NotchFix.patch.txt
 cd -
 
 echo " ===+++ Building Recovery +++==="
 rm -rf out
 source build/envsetup.sh
 echo " source build/envsetup.sh done"
-#export ALLOW_MISSING_DEPENDENCIES=true
+export ALLOW_MISSING_DEPENDENCIES=true
 lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
 echo " lunch omni_${DEVICE}-eng done"
 mka recoveryimage || abort " mka failed with exit status $?"
