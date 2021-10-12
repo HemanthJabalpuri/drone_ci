047e7f8e25ac30753359c3c1d84adb8d7a8c34b8
Hemanth <hemanthjabalpuri@gmail.com>
2021-10-12 08:49:07 +0530

    Test ArrowOS building

--- build.sh
+++ build.sh
@@ -3,6 +3,20 @@
 
 abort() { echo "$1"; exit 1; }
 
+repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-device,-mips,-darwin,-notdefault
+git clone https://github.com/HemanthJabalpuri/local_manifest --depth 1 -b master .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom (4)
+source build/envsetup.sh
+lunch arrow_RMX2185-userdebug
+
+make sepolicy
+make bootimage
+make init
+
+echo ".......Done......." && exit
+
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
 
 echo " ===+++ Setting up Build Environment +++==="
