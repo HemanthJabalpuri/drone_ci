41768025cd95be1f5c8c9f1f50525ec5b3d3047c
Hemanth <hemanthjabalpuri@gmail.com>
2021-10-26 12:43:36 +0530

    Realme C12 nocrypt with twrp11

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 abort() { echo "$1"; exit 1; }
 
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
-DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX3195 -b test"
-DT_PATH=device/realme/RMX3195
+DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX2185 -b test"
+DT_PATH=device/realme/RMX2185
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
@@ -28,7 +28,8 @@ applyPatch() {
   [ $? != 0 ] && echo " Patch $1 failed" && exit
 }
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/6992094/0001-Provide-an-option-to-skip-compatibility.zip-check.patch-a11.txt
-applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7350748/0001-String-fixes-and-added-some-shell-functions-a11.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7415929/0001-String-fixes.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7415933/0001-Some-shell-funtions.patch.txt
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7350752/0001-Super-as-Super-only.patch-a11.txt
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/6991161/NotchFix.patch.txt
 cd -
