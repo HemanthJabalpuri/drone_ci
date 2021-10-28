cbc6c3f7d2f0c8409c2422758cc8a41bed5e4ff5
Hemanth <hemanthjabalpuri@gmail.com>
2021-10-28 19:50:04 +0530

    Infinix S4

--- build.sh
+++ build.sh
@@ -5,15 +5,15 @@ PBRP=n
 
 abort() { echo "$1"; exit 1; }
 
-DT_PATH=device/infinix/X690
+DT_PATH=device/infinix/X626
 if [ "$PBRP" = "y" ]; then
   REC=PBRP
   MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-10.0"
   DT_LINK="https://github.com/HemanthJabalpuri/twrp_infinix_X687 -b pbrp"
 else
   REC=TWRP
-  MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
-  DT_LINK="https://github.com/HemanthJabalpuri/twrp_infinix_X690"
+  MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0"
+  DT_LINK="https://github.com/HemanthJabalpuri/twrp_infinix_X626"
 fi
 DEVICE=${DT_PATH##*\/}
 
