52fafd2e8441ff77adccd9fd4871e0d373e7b698
Hemanth <hemanthjabalpuri@gmail.com>
2021-11-15 08:01:44 +0530

    Realme C25 TWRP v3.6.0 release

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 abort() { echo "$1"; exit 1; }
 
 MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
-DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX3195 -b android-11"
-DT_PATH=device/realme/RMX3195
+DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX3191 -b android-11"
+DT_PATH=device/realme/RMX3191
 
 echo " ===+++ Setting up Build Environment +++==="
 apt install openssh-server -y
