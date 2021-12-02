26be481af05d47a593a6f9960a9aa5b8903b909b
Hemanth <hemanthjabalpuri@gmail.com>
2021-12-02 17:45:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ apt update --fix-missing
 apt install openssh-server -y
 
 repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-11.0 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/HemanthJabalpuri/local_manifest --depth 1 -b arrow-11-UI1 .repo/local_manifests
+git clone https://github.com/HemanthJabalpuri/local_manifest --depth 1 -b arrow .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom (5-test)
