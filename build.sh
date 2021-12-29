b8ab56a0aa059ba9dcaab2a05df26596b9933a94
Hemanth <hemanthjabalpuri@gmail.com>
2021-12-29 09:20:20 +0530

    Infinix Smart 3 Plus

--- build.sh
+++ build.sh
@@ -5,15 +5,15 @@ PBRP=n
 
 abort() { echo "$1"; exit 1; }
 
-DT_PATH=device/Lenovo/TB8505F
+DT_PATH=device/infinix/Nova
 if [ "$PBRP" = "y" ]; then
   REC=PBRP
   MANIFEST="git://github.com/PitchBlackRecoveryProject/manifest_pb.git -b android-11.0"
   DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX3191 -b pbrp"
 else
   REC=TWRP
-  MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0-deprecated"
-  DT_LINK="https://github.com/HemanthJabalpuri/twrp_Lenovo_TB8505F"
+  MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0"
+  DT_LINK="https://github.com/HemanthJabalpuri/twrp_infinix_X627 -b test"
 fi
 DEVICE=${DT_PATH##*\/}
 
@@ -38,7 +38,7 @@ applyPatch() {
   curl -sL $1 | patch -p1
   [ $? != 0 ] && echo " Patch $1 failed" && exit
 }
-applyPatch https://github.com/TeamWin/android_bootable_recovery/commit/878abc76c26e01e98c9b820c143b51086fe1577c.patch
+#applyPatch https://github.com/TeamWin/android_bootable_recovery/commit/878abc76c26e01e98c9b820c143b51086fe1577c.patch
 #applyPatch https://github.com/TeamWin/android_bootable_recovery/commit/878abc76c26e01e98c9b820c143b51086fe1577c.patch
 #applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2194/files/6997950/SkipTrebleCompatibility.patch.txt
 #applyPatch https://github.com/HemanthJabalpuri/android_recovery_realme_RMX2185/files/6694299/0001-Super-as-Super-only.patch.txt
