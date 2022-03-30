a99f6c020997111f4aa80482f00a7e66ff8050e4
Hemanth <hemanthjabalpuri@gmail.com>
2022-03-30 22:46:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ apt update --fix-missing
 apt install openssh-server -y
 
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/HemanthJabalpuri/local_manifest --depth 1 -b crdroid-11-UI1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
