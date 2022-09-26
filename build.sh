17bf880abcb7876cc8a90d74f6a0bbfccfb32d91
Hemanth <hemanthjabalpuri@gmail.com>
2022-09-26 21:41:42 +0530

    nocrypt-twrp11 C12

--- build.sh
+++ build.sh
@@ -27,12 +27,10 @@ applyPatch() {
   curl -sL $1 | patch -p1
   [ $? != 0 ] && echo " Patch $1 failed" && exit
 }
-#applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/8974877/0001-Provide-an-option-to-skip-compatibility.zip-check.patch.txt
-#applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2194/files/6997950/SkipTrebleCompatibility.patch.txt
-applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/8974880/0001-String-fixes.patch.txt
-applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/8974878/0001-Some-shell-funtions.patch.txt
-applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/8974881/0001-Super-as-Super-only.patch.txt
-applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/8974882/NotchFix.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7415929/0001-String-fixes.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7415933/0001-Some-shell-funtions.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7350752/0001-Super-as-Super-only.patch-a11.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/6991161/NotchFix.patch.txt
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/9647625/RemoveDataFlags.patch.txt
 cd -
 
