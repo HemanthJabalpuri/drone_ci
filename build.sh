c5632fa5f40892ec539c147613b5a82b3eda735b
Hemanth <hemanthjabalpuri@gmail.com>
2022-10-03 22:30:52 +0530

    C12 UI1-twrp11 test
    Release I guess

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 
 abort() { echo "$1"; exit 1; }
 
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1"
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
 DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX2185 -b android-10.0"
 DT_PATH=device/realme/RMX2185
 
