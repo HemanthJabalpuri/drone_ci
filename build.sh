c641bd0a4f9cd565db5f76e467ca3aaf8d13a806
Hemanth <hemanthjabalpuri@gmail.com>
2022-10-03 14:40:36 +0530

    nocrypt-twrp-11 C12 test
    Ready to release I guess

--- build.sh
+++ build.sh
@@ -30,6 +30,7 @@ applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7415929
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/9694955/0001-Some-shell-funtions.patch.txt
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7350752/0001-Super-as-Super-only.patch-a11.txt
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/9680977/changeNavbarLayout.patch.txt
+applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/9696114/0001-RemoveNoOSinstalledWarning.patch.txt
 cd -
 
 echo " ===+++ Building Recovery +++==="
