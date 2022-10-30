0e6f340f346dc1fe3e238c3d9cb3ac6b33785565
Hemanth <hemanthjabalpuri@gmail.com>
2022-10-30 13:53:31 +0530

    twrp-12.1 C12 test

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 
 abort() { echo "$1"; exit 1; }
 
-MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-11"
-DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX2185 -b android-11"
+MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1"
+DT_LINK="https://github.com/HemanthJabalpuri/twrp_realme_RMX2185"
 DT_PATH=device/realme/RMX2185
 
 echo " ===+++ Setting up Build Environment +++==="
@@ -16,7 +16,6 @@ DEVICE=${DT_PATH##*\/}
 echo " ===+++ Syncing Recovery Sources +++==="
 repo init --depth=1 -u $MANIFEST
 repo sync
-git clone --depth=1 $DT_LINK $DT_PATH
 
 echo " ===+++ Patching Recovery Sources +++==="
 #rm -rf bootable/recovery
@@ -26,6 +25,7 @@ applyPatch() {
   curl -sL $1 | patch -p1
   [ $? != 0 ] && echo " Patch $1 failed" && exit
 }
+applyPatch https://github.com/HemanthJabalpuri/android_bootable_recovery/commit/22e9c22965d25247f809e9325364e174f13ddf0f.patch
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7415929/0001-String-fixes.patch.txt
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/9694955/0001-Some-shell-funtions.patch.txt
 applyPatch https://github.com/HemanthJabalpuri/twrp_realme_RMX2185/files/7350752/0001-Super-as-Super-only.patch-a11.txt
@@ -37,26 +37,45 @@ echo " ===+++ Building Recovery +++==="
 export ALLOW_MISSING_DEPENDENCIES=true
 . build/envsetup.sh
 echo " source build/envsetup.sh done"
-lunch twrp_${DEVICE}-eng || abort " lunch failed with exit status $?"
-echo " lunch twrp_${DEVICE}-eng done"
-mka recoveryimage || abort " mka failed with exit status $?"
-echo " mka recoveryimage done"
 
-# Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
-echo " ===+++ Uploading Recovery +++==="
 version=$(cat bootable/recovery/variables.h | grep "define TW_MAIN_VERSION_STR" | cut -d \" -f2)
-#OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
-OUTFILE=TWRP-${version}-${DEVICE}-UI2-$(date "+%Y%m%d").zip
+mkdir uploads
 
-cd out/target/product/$DEVICE
-mv recovery.img ${OUTFILE%.zip}.img
-zip -r9 $OUTFILE ${OUTFILE%.zip}.img
+build_twrp() {
+  rm -rf $DT_PATH
+  git clone --depth=1 $DT_LINK -b $1 $DT_PATH
+  lunch twrp_${DEVICE}-eng || abort " lunch failed with exit status $?"
+  echo " lunch twrp_${DEVICE}-eng done"
+  mka clobber
+  export TW_DEVICE_VERSION="$2"
+  mka recoveryimage || abort " mka failed with exit status $?"
+  echo " mka recoveryimage done"
 
-#curl -T $OUTFILE https://oshi.at
-curl -F "file=@${OUTFILE}" https://file.io
-#curl --upload-file $OUTFILE http://transfer.sh/
-curl bashupload.com -T $OUTFILE
-echo " "
-curl -sL https://git.io/file-transfer | sh
-./transfer wet $OUTFILE
-echo " "
+  echo " ===+++ Moving Recovery +++==="
+  #OUTFILE=TWRP-${version}-${DEVICE}-$(date "+%Y%m%d-%I%M").zip
+  OUTFILE=TWRP-${version}-${3}-${DEVICE}-$(date "+%Y%m%d").zip
+
+  cd out/target/product/$DEVICE
+  mv recovery.img ${OUTFILE%.zip}.img
+  zip -r9 $OUTFILE ${OUTFILE%.zip}.img
+  cd -
+  cp out/target/product/$DEVICE/$OUTFILE uploads/
+}
+
+build_twrp android-11 "1" "1-UI2"
+build_twrp android-10.0 "1" "1-UI1"
+
+ls -lR uploads
+for i in ./uploads/*.zip; do
+  # Upload zips & recovery.img (U can improvise lateron adding telegram support etc etc)
+  #curl -T $i https://oshi.at
+  curl -F "file=@${i}" https://file.io
+  #curl --upload-file $i http://transfer.sh/
+  curl bashupload.com -T $i
+  echo " "
+  curl -sL https://git.io/file-transfer | sh
+  ./transfer wet $i
+  echo " "
+done
+
+echo " Done"
